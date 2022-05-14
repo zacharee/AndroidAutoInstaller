9a6cf238d2453940b2231b97873296e022748848
Zachary Wander <zachary.wander@gmail.com>
2022-05-14 13:56:24 -0400

    Add icon and update colors

--- app/src/main/java/dev/zwander/androidautoinstaller/ui/theme/Color.kt
+++ app/src/main/java/dev/zwander/androidautoinstaller/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package dev.zwander.androidautoinstaller.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Purple200 = Color(0xFF76d275)
+val Purple500 = Color(0xFF43a047)
+val Teal200 = Color(0xFF1976d2)
